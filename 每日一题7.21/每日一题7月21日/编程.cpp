993fa9957623fa85f236118ceed38570d687dba4
Angelay2 <y2zaijian@126.com>
2020-08-31 23:50:41 +0800

    乒乓球筐

--- 每日一题7.21/每日一题7月21日/编程.cpp
+++ 每日一题7.21/每日一题7月21日/编程.cpp
@@ -43,3 +43,43 @@ int main() {
 	}
 	return 0;
 }
+/*
+2. 骆驼命名法
+从C/C++转到Java的程序员，一开始最不习惯的就是变量命名方式的改变。C语言风格使用下划线分隔多个单 词，例如“hello_world”；而Java则采用一种叫骆驼命名法的规则：除首个单词以外，所有单词的首字母大写， 例如“helloWorld”。 请你帮可怜的程序员们自动转换变量名。 输入描述： 输入包含多组数据。 每组数据一行，包含一个C语言风格的变量名。每个变量名长度不超过100。 输出描述： 对应每一组数据，输出变量名相应的骆驼命名法。
+【解题思路】：
+首先一个字符一个字符的读取内容
+1. 遇到 _ 就直接跳过
+2. 如果上一个字符是 _ 则下一个字符转大写字母
+*/
+#include<iostream> 
+#include<vector> 
+#include<string> 
+#include<algorithm>
+#include<functional> 
+#include <map>
+#include <set> 
+#include <unordered_set>
+#include <unordered_map>
+#include <exception> 
+#include <iomanip>
+#include <memory> 
+#include <sstream> 
+#define INF 1000000
+using namespace std;
+int main(int argc, char** argv) {
+	string s;
+	while (cin >> s) { 
+		for (int i = 0; i < s.size(); ++i) {
+			// 1. 遇到 _ 就直接跳过
+			if (s[i] == '_') 
+				continue; 
+			// 2. 如果上一个字符是 _ 则下一个字符转为大写字母
+			if (i > 0 && s[i - 1] == '_') 
+				cout << (char)toupper(s[i]);
+			else 
+				cout << s[i];
+		}
+		cout << endl;
+	}
+	return 0; 
+}
